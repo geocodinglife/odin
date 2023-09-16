4432e38ce177a283d38351ea95872f34b4da63c1
geocodinglife <geocodinglife@gmail.com>
2023-09-16 17:14:05 -0500

    Chat belongs to one lead

--- app/models/user_room.rb
+++ app/models/user_room.rb
@@ -1,12 +1,12 @@
 class UserRoom < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  
+
   after_commit on: :create do
     broadcast_append_to(
-      'user_rooms_channel',
-      partial: 'shared/room',
-      locals: { room: Room.find(room_id) },
+      "user_rooms_channel",
+      partial: "shared/room",
+      locals: {room: Room.find(room_id)},
       target: "user_#{user_id}_rooms"
     )
   end
