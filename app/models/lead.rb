1902c03034c6c59f2dfd0c99212075d71cd76163
geocodinglife <geocodinglife@gmail.com>
2023-09-16 20:01:51 -0500

    Add URL

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -17,7 +17,7 @@ class Lead < ApplicationRecord
     request["Token"] = ENV["HABLAME_TOKEN"]
     request["Content-Type"] = "application/json"
     request["Accept"] = "application/json"
-    request.body = "{\n  \"toNumber\": \"573203850750\",\n  \"sms\": \"SMS de prueba Hablame\",\n  \"flash\": \"0\",\n  \"sc\": \"890202\",\n  \"request_dlvr_rcpt\": \"0\"\n}"
+    request.body = "{\n  \"toNumber\": \"573203850750\",\n  \"sms\": \"You got new Lead www.odin.zone/rooms  \",\n  \"flash\": \"0\",\n  \"sc\": \"890202\",\n  \"request_dlvr_rcpt\": \"0\"\n}"
 
     # this is the correct code to by use in production
     # request.body = "{\n  \"toNumber\": \"57#{seller_phone}\",\n  \"sms\": \"SMS de prueba Hablame\",\n  \"flash\": \"0\",\n  \"sc\": \"890202\",\n  \"request_dlvr_rcpt\": \"0\"\n}"
