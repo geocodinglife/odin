d90d586c4b2a68515e0a272d79eda65563095f12
geocodinglife <geocodinglife@gmail.com>
2023-10-03 22:44:24 -0500

    clened the code in the welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,21 @@
 class WelcomeController < ApplicationController
   def index
+    @icons = {
+      "Mobiles" => "mobile.png",
+      "Vehiculos" => "car.png",
+      "Propiedades" => "property.png",
+      "Alquileres" => "rental.png",
+      "Electronica" => "electro.png",
+      "Bicicletas" => "bikes.png",
+      "Empresas" => "business.png",
+      "Servicios" => "services.png",
+      "Empleos" => "jobs.png",
+      "Mascotas" => "pets.png",
+      "Muebles" => "furniture.png",
+      "Libros" => "books.png",
+      "Niños" => "kids.png"
+    }
+
     @products = Product.last(40)
   end
 end
