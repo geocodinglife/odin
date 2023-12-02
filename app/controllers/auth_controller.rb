0e93f579e2f111f745f5cfbcf4ab7fe059da3598
geocodinglife <geocodinglife@gmail.com>
2023-12-02 12:29:26 -0500

    debuging

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -7,6 +7,11 @@ class AuthController < ApplicationController
   def create
     session[:phone] = params[:phone]
     session[:salt] = UserLogin.start_auth(params)
+    p "*" * 125
+    p "I arribed to the authenticatin ."
+    p session[:phone]
+    p session[:salt]
+    p "*" * 125
     redirect_to auth_verifications_path
   rescue ActiveRecord::RecordInvalid
     render :show
