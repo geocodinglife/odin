618dda1255f6d47093e1aef8309d7944e748d4d7
geocodinglife <geocodinglife@gmail.com>
2023-12-02 14:23:29 -0500

    change conditional to create user

--- app/models/user_login.rb
+++ app/models/user_login.rb
@@ -5,10 +5,7 @@ module UserLogin
     salt = User.generate_auth_salt
     user = User.find_by(phone: params[:phone])
 
-    if !user.nil?
-      User.send_login_code_message_to_user(user, user.auth_code(salt))
-      salt
-    else
+    if user.nil?
       user = User.create!(email: params[:email],
         first_name: params[:first_name],
         phone: params[:phone],
@@ -16,6 +13,7 @@ module UserLogin
     end
 
     User.send_login_code_message_to_user(user, user.auth_code(salt))
+
     salt
   end
 
