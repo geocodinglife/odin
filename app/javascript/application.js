f88aa0a97fd8c46a0184df3869381531685aee4d
geocodinglife <geocodinglife@gmail.com>
2023-12-04 20:45:43 -0500

    drop down working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -22,3 +22,15 @@ import "@fortawesome/fontawesome-free"
 // import './components/alert/evita-robos.js';
 // import './components/alert/alerta-pin.js';
 // import './components/alert/card-producto.js';
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
