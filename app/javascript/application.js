899765abf2f749b42cd5cbc3a12a638675fb5fc8
geocodinglife <geocodinglife@gmail.com>
2023-12-04 20:54:45 -0500

    Burger button working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,32 +5,18 @@ import "trix"
 import "@rails/actiontext"
 import "@fortawesome/fontawesome-free"
 
-// new Code.
 
-// import './components/alert/alerta-registro.js';
-// import './components/alert/alerta-inicio-sesion.js';
-// import './components/alert/alerta-vender.js';
-// import './components/alert/alerta-vender-dos.js';
-// import './components/alert/creado-exitosamente.js';
-// import './components/alert/menu-nav-opciones.js';
-// import './components/info/footer-component.js';
-// import './components/info/productos-para-ti.js'
-// import './components/alert/alerta-notificara-vendedor.js';
-// import './components/alert/nuevo-cliente.js';
-// import './components/alert/nuevo-cliente-dos.js';
-// import './components/alert/nueva-tarjeta.js';
-// import './components/alert/evita-robos.js';
-// import './components/alert/alerta-pin.js';
-// import './components/alert/card-producto.js';
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+document.addEventListener("turbo:load", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }));
+});
