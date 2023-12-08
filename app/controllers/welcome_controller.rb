1587a00e99bdd5fec0a268f4fac015fbf092b1d5
geocodinglife <geocodinglife@gmail.com>
2023-12-07 21:02:35 -0500

    fixing the space

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,10 @@
 class WelcomeController < ApplicationController
   def index
     @products = Product.includes(:images_attachments).last(40)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 end
