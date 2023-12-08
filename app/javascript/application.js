54bdb293e41219b4ffd751f41934c237e15ad40f
geocodinglife <geocodinglife@gmail.com>
2023-12-08 12:12:21 -0500

    removed garbage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
-import "@fortawesome/fontawesome-free"
 
 
 document.addEventListener("turbo:load", () => {
