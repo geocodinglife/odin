91d06ef268f55d2b0ed232f5c4bebe5e2ebb87a8
geocodinglife <geocodinglife@gmail.com>
2023-12-24 07:57:21 -0500

    finding the bug in heroku

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/actiontext", to: "actiontext.js", preload: true
-pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.7/dist/trix.esm.min.js", preload: true
+pin "trix", to: "https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js", preload: true
 
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
