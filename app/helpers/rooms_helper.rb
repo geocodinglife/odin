fb1a69671f35e3dd9aa5fe3efa46881d994d7bd2
geocodinglife <geocodinglife@gmail.com>
2024-01-02 20:24:27 -0500

    add new integrety

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -2,8 +2,6 @@ module RoomsHelper
   require "digest"
 
   def generate_wompi_signature(reference, amount, currency, wompi_integrity)
-    original_data = "3b4393bafed398ba1"
-    new_reference = original_data.chars.shuffle.join  + reference
-    Digest::SHA2.hexdigest(new_reference + amount + currency + wompi_integrity)
+    Digest::SHA2.hexdigest(reference + amount + currency + wompi_integrity)
   end
 end
