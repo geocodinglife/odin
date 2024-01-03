ef401734d741c2f8fedea47ab873e6a57b7053ed
geocodinglife <geocodinglife@gmail.com>
2024-01-02 19:47:07 -0500

    trying to fix the bug

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -2,7 +2,6 @@ module RoomsHelper
   require "digest"
 
   def generate_wompi_signature(reference, amount, currency, wompi_integrity)
-    # binding.break
     Digest::SHA2.hexdigest(reference + amount + currency + wompi_integrity)
   end
 end
